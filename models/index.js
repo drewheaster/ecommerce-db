f5ff9cb71a284ff9935fe760cf20229b5a954d42
Drew Easter <drewheaster@gmail.com>
2024-09-04 23:49:31 -0500

    created API routes for each model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Category, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  
+  as: 'category_products'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  
+  as: 'product_tags'
+});
+
 
 module.exports = {
   Product,
